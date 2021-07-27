06d25dbf7ca45671e0b459968a3e3b47b58bb062
Brent Lopez <brent@brentlopez.me>
2021-07-27 13:20:44 -0400

    Make gmail access actually async

--- daily-ui-email-parser.js
+++ daily-ui-email-parser.js
@@ -1,13 +1,14 @@
-import {init} from './gmail-plugin.js';
+import { init } from './gmail-plugin.js';
 import * as jsdom from 'jsdom';
 import { gmail_v1 } from 'googleapis';
+import * as fs from 'fs';
 
 
-const {JSDOM} = jsdom;
+const { JSDOM } = jsdom;
 var mess = [];
 
-export async function run(callback){
-    getDailyUiMessages(await init(), callback);
+export async function run(callback) {
+  await getDailyUiMessages(await init(), callback);
 }
 
 /**
@@ -15,146 +16,178 @@ export async function run(callback){
  *
  * @param {gmail_v1} gmail An authorized OAuth2 client.
  */
- function getDailyUiMessages(gmail, callback) {
-    gmail.users.messages.list({
-      userId: 'me',
-      q: 'subject:("Daily UI ::") from:hello@dailyui.co'
-    }, async (err, res) => {
-      if (err) return console.log('The API returned an error: ' + err);
-      const messages = res.data.messages;
-      if (messages.length) {
-        console.log('Messages:');
-    
-        for (let i = 0; i < messages.length; i++) {
-          const message = messages[i];
-          await sleep(50);
-          gmail.users.messages.get(
-            {userId: 'me', id: message.id},
-            (err1, res1) => {
-              if (err1) return console.log('The API returned an error: ' + err);
-  
-              var subj = getDailyUiNumber(res1.data);            
-              if((subj === -1) && !(i == messages.length - 1)) return;
-  
-              if(subj && subj.length > 0){
-                mess.push(res1.data);
-              }
-  
-              if(i == messages.length - 1) {
-                mess = mess.sort((a, b) => {
-                  return parseInt(getDailyUiNumber(a)) - parseInt(getDailyUiNumber(b));
-                });
-      
-                //mess.forEach(m => console.log(getGalleryDataFromMessage(m)));  
-                callback(Array.from(mess, getGalleryDataFromMessage))            
-              }
-          });
-        }
-      } else {
-        console.log('No messages found.');
-      }
-    });
+async function getDailyUiMessages(gmail, callback) {
+  mess = await getCachedMessages();
+  if (mess.length > 0) {
+    callback(Array.from(mess, getGalleryDataFromMessage))
+    return;
   }
-  
-  function getSubject(message){
-   return message.payload.headers.find(h => h.name === "Subject").value;
+
+  var messageList = await gmail.users.messages.list({
+    userId: 'me',
+    q: 'subject:("Daily UI ::") from:hello@dailyui.co'
+  })
+    .catch(err => console.log('The API returned an error: ' + err));
+
+  if(!messageList) return;
+
+  const messages = messageList.data.messages;
+  if (messages.length) {
+    console.log('Messages:');
+
+    for (let i = 0; i < messages.length; i++) {
+      const message = messages[i];
+      await sleep(50);
+      var fullMessage = await gmail.users.messages.get(
+        { userId: 'me', id: message.id }
+      )
+        .catch(err => console.log('The API returned an error: ' + err));
+
+      if(!fullMessage) return;
+
+
+      var subj = getDailyUiNumber(fullMessage.data);
+      if ((subj === -1) && !(i == messages.length - 1)) return;
+
+      if (subj && subj.length > 0) {
+        mess.push(fullMessage.data);
+      }
+
+      if (i == messages.length - 1) {
+        mess = mess.sort((a, b) => {
+          return parseInt(getDailyUiNumber(a)) - parseInt(getDailyUiNumber(b));
+        });
+
+        //mess.forEach(m => console.log(getGalleryDataFromMessage(m)));  
+        const gallery = Array.from(mess, getGalleryDataFromMessage);
+        cacheMessages(gallery);
+        callback(gallery)
+      }
+    }
+  } else {
+    console.log('No messages found.');
   }
-  
-  function getDailyUiNumber(message){
-   const subj = getSubject(message).replace( /^\D+/g, '');
-    if(subj.length >= 3){
-      return subj.substr(0, 3);
-    }
-  
-    return -1;
+}
+
+function getSubject(message) {
+  return message.payload.headers.find(h => h.name === "Subject").value;
+}
+
+function getDailyUiNumber(message) {
+  const subj = getSubject(message).replace(/^\D+/g, '');
+  if (subj.length >= 3) {
+    return subj.substr(0, 3);
   }
-  
-  function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  } 
-  
-  function getGalleryDataFromMessage(message){
-    const body = message.payload;
-  
-    var thing = readOgMessages(message)
-    if(thing){
-      return thing;
-    } else {
-      var htmlBody = body.parts.find(p => p.mimeType === 'text/html').body.data;
-  
-      var dom = new JSDOM(Buffer.from(htmlBody, 'base64'));
-  
-      const document = dom.window.document;
-      const bodyEl = document.body;
-  
-      const titleEl = bodyEl.querySelector('h1 span span:nth-of-type(2)');
-      const promptEl = bodyEl.querySelector('span > span > em');
-  
-      return {
-        prommpt: promptEl?.innerHTML,
-        title: titleEl?.innerHTML,
-        number: getDailyUiNumber(message)
-      };
-    }
-  
-    
-  }
-  
-  function readOgMessages(message){
-    if(parseInt(getDailyUiNumber(message)) === 51){
-      console.log('');
-    }
-    const body = message.payload;
-    var html = body.parts.find(p => p.mimeType === 'text/plain').body.data;
-    let buff = Buffer.from(html, 'base64');
-    html = buff.toString();
-  
-    var lines = html.split(/(?:\r\n|\r|\n)/g);
-  
-    let readTitle = false;
-    let readPrompt = false;
-  
-    let title;
-    let prompt;
-  
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
-      
-  
-      if(title && prompt){
-        break;
-      }
-  
-      if(!line || line.length === 0) continue;
-  
-      if(readTitle){
-        readTitle = false;
-        title = line;
-      }
-  
-      if(readPrompt){
-        readPrompt = false;
-        prompt = line;
-      }
-  
-      if(line.includes('**')){
-        readTitle = true;
-      }
-  
-      if(line.includes("Design Hint")){
-        readPrompt = true;
-      }
-    }
-  
-    if(!title || !prompt){
-      return null;
-    }
-  
+
+  return -1;
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function getGalleryDataFromMessage(message) {
+  const body = message.payload;
+
+  var thing = readOgMessages(message)
+  if (thing) {
+    return thing;
+  } else {
+    var htmlBody = body.parts.find(p => p.mimeType === 'text/html').body.data;
+
+    var dom = new JSDOM(Buffer.from(htmlBody, 'base64'));
+
+    const document = dom.window.document;
+    const bodyEl = document.body;
+
+    const titleEl = bodyEl.querySelector('h1 span span:nth-of-type(2)');
+    const promptEl = bodyEl.querySelector('span > span > em');
+
     return {
-      prommpt: prompt,
-      title: title,
+      prommpt: promptEl?.innerHTML,
+      title: titleEl?.innerHTML,
       number: getDailyUiNumber(message)
     };
   }
+
+
+}
+
+function readOgMessages(message) {
+  if (parseInt(getDailyUiNumber(message)) === 51) {
+    console.log('');
+  }
+  const body = message.payload;
+  var html = body.parts.find(p => p.mimeType === 'text/plain').body.data;
+  let buff = Buffer.from(html, 'base64');
+  html = buff.toString();
+
+  var lines = html.split(/(?:\r\n|\r|\n)/g);
+
+  let readTitle = false;
+  let readPrompt = false;
+
+  let title;
+  let prompt;
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+
+
+    if (title && prompt) {
+      break;
+    }
+
+    if (!line || line.length === 0) continue;
+
+    if (readTitle) {
+      readTitle = false;
+      title = line;
+    }
+
+    if (readPrompt) {
+      readPrompt = false;
+      prompt = line;
+    }
+
+    if (line.includes('**')) {
+      readTitle = true;
+    }
+
+    if (line.includes("Design Hint")) {
+      readPrompt = true;
+    }
+  }
+
+  if (!title || !prompt) {
+    return null;
+  }
+
+  return {
+    prommpt: prompt,
+    title: title,
+    number: getDailyUiNumber(message)
+  };
+}
+
+function cacheMessages(gallery) {
+  fs.promises.writeFile('.cache', JSON.stringify(gallery))
+    .then(() => {
+      console.log('Cached messages stored in .cache')
+    });
+}
+
+async function getCachedMessages() {
+  return await fs.promises.readFile('.cache', 'ascii')
+    .then(cache => {
+      console.log('Caching messages in .cache')
+      mess = JSON.parse(cache);
+      return mess;
+    })
+    .catch(err => {
+      console.log('No .cache found.')
+      return [];
+    })
+}
